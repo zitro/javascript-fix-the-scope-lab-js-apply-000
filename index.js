335eaec89c8faf609e523cc9c50c366f95045490
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-10 18:10:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,10 +8,8 @@ animal == myAnimal;
 
 function yourAnimal(animal) {
 animal == yourAnimal;
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
   return yourAnimal
+}
 
 function add2(n) {
 const two = 2;
